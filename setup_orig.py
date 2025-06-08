bc2cc485f7c800e9ceb9dd9756258f8546fc2630
1658218630 <1652818630@qq.com>
2025-06-08 20:18:45 +0200

    最后确定是cuda版本问题，升级cuda前保存一下

--- setup_orig.py
+++ setup_orig.py
@@ -26,9 +26,7 @@ setup(
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/"),
-                                         "-allow-unsupported-compiler"       # ← 新增这一行
-                                         ]})
+            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
         ],
     cmdclass={
         'build_ext': BuildExtension
